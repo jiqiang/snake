99f543800d8818690b90936435d23df9b49aab12
Qiang Ji <qiang.j@gmail.com>
2020-03-06 20:03:33 +1100

    setup github pages

---
+++ docs/precache-manifest.9e446765ca6c93d6391ff4462bf72dfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19549441ae9226270cf1b3dd868c27de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "777deaac74f5a50c9041",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "611125aa255f99b96609",
+    "url": "/static/css/main.7b9c1d81.chunk.css"
+  },
+  {
+    "revision": "777deaac74f5a50c9041",
+    "url": "/static/js/2.99f81b22.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.99f81b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "611125aa255f99b96609",
+    "url": "/static/js/main.a85ab688.chunk.js"
+  },
+  {
+    "revision": "ba4ded59d726c9505166",
+    "url": "/static/js/runtime-main.fad7d2df.js"
+  }
+]);
